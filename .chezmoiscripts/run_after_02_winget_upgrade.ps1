bb29e598b6aa9fb1df13fb0cd8ea84e02c642afb
Keisuke Hirota <14054951+k-tahiro@users.noreply.github.com>
2026-07-16 08:02:40 +0900

    Remove .chezmoidata/winget.toml Remove .chezmoiscripts/02_winget_upgrade.ps1 Remove .chezmoiscripts/01_winget.ps1 Remove .config/winget/packages.json

--- .chezmoiscripts/run_after_02_winget_upgrade.ps1
+++
@@ -1,8 +0,0 @@
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Error "Error: winget is not installed."
-    exit 1
-}
-
-Write-Host "Upgrading all winget packages..."
-winget upgrade -r --accept-package-agreements --accept-source-agreements
-Write-Host "Winget upgrade complete."
